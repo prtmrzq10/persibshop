2da833285f0665609018e60be106fb5818332ed0
prtmrzq10 <riziqfajri7@gmail.com>
2024-08-05 14:41:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+    document.querySelector('.navbar-toggle').addEventListener('click', () => {
+        document.querySelector('.navbar-menu').classList.toggle('active');
+    });
+</script>
